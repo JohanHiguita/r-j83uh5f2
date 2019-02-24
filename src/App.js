7da323a60d138c6fe080b40dcbd275ac22796872
Johan Higuita <higuitajohan@gmail.com>
2019-02-23 23:08:53 -0500

    passed test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { counter: 0 };
+    /* this.updateCounter = this.updateCounter.bind(this); */
+  }
+  
+
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.updateCounter.bind(this)} ></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
+
+  updateCounter(evt) {
+    this.setState({
+      counter: evt.target.value.length
+    });
+  }
+
+
+
 }
 
 export default App;
